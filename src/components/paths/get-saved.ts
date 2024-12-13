04260cafad94126bd255e65b9bf42717abe49d79
marcog83 <marcogobbi0@gmail.com>
2024-12-13 23:49:38 +0100

    all the changes

--- src/components/paths/get-saved.ts
+++ src/components/paths/get-saved.ts
@@ -1,9 +1,17 @@
 // import routes from './saved-routes.csv?raw';
+import Papa from 'papaparse';
 import routes from './suggested_routes.csv?raw';
+// 650,4,La Todolella,Morron
+type Route = {
+  id:number,
+  weight:number,
+  sourceName:string,
+  destName:string
+};
+// Parse the CSV into a structured array of objects
+const parseResult = Papa.parse<Route>(routes, {
+  header: true,
+  skipEmptyLines: true,
+});
 
-export const getSavedRoutes = () => routes.split('\n')
-  .slice(1)
-  .map((line) => {
-    const [ id ] = line.split(',');
-    return parseInt(id, 10);
-  });
+export const getSavedRoutes = () => parseResult.data.map(({ id }) => parseInt(id, 10));
